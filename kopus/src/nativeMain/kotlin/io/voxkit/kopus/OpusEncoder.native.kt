0e243773cf0ac9e88b9ae53c7c2d5aaf398037a4
Alex Shepeliev <a.shepeliev@gmail.com>
2025-07-18 13:08:38 +0300

    Implement native

--- kopus/src/nativeMain/kotlin/io/voxkit/kopus/OpusEncoder.native.kt
+++ kopus/src/nativeMain/kotlin/io/voxkit/kopus/OpusEncoder.native.kt
@@ -1,6 +1,10 @@
 package io.voxkit.kopus
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.toKString
+import opus.opus_get_version_string
+
 public actual class OpusEncoder {
-    public actual val version: String
-        get() = TODO("Not yet implemented")
+    @OptIn(ExperimentalForeignApi::class)
+    public actual val version: String = opus_get_version_string()?.toKString() ?: "0.0.0"
 }
